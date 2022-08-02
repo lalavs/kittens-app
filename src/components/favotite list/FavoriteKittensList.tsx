0a72534f0a8a0b08b365cde61d47b057e5f1d9b8
lalavs <lalaveselova@gmail.com>
2022-08-02 16:24:04 +0300

    list of favorite kittens

--- src/components/favotite list/FavoriteKittensList.tsx
+++ src/components/favotite list/FavoriteKittensList.tsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { FC } from 'react';
+import { observer } from 'mobx-react-lite';
+import {FaHeart} from 'react-icons/fa';
 
-const FavoriteKittensList = () => {
+import store, { IKitten } from '../../store/Kittens';
+import '../general list/KittensList';
+
+const FavoriteKittensList: FC = observer(() => {
+  const handleClick = (item: IKitten): void => {
+    store.removeFromFavoriteKittens(item.id);
+  };
+
   return (
-    <div>FavoriteKittens</div>
+    <div className='kittens-list'>
+      {store.favoriteKittens.map((item: IKitten) => {
+        return (
+          <div key={item.id} className='item'>
+          <img
+            src={item.url} 
+            className='item-img' 
+            alt=''
+          />
+          <div className='item-overlay'>
+            <div className='icons'>
+              <FaHeart
+                className='icon-active'
+                onClick={() => handleClick(item)}
+              />
+            </div>
+          </div>
+        </div>
+        )}
+      )}
+    </div>
   );
-};
+});
 
 export default FavoriteKittensList;
