4881baf7c1e93f589f192c01cb8ab1dfaef621d1
lalavs <lalaveselova@gmail.com>
2022-08-02 16:57:09 +0300

    infinite scroll

--- src/components/general list/KittensList.tsx
+++ src/components/general list/KittensList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { observer } from 'mobx-react-lite';
 import axios from 'axios';
 
@@ -7,17 +7,36 @@ import './KittensList.scss';
 import KittenItem from '../item/KittenItem';
 
 const KittensList = observer(() => {
+  const [currentPage, setCurrentPage] = useState<number>(0);
+  const [fetching, setFetching] = useState<boolean>(true);
+
+  const scrollHandler = (): void => {
+    if (document.documentElement.scrollHeight - (document.documentElement.scrollTop + window.innerHeight) < 60) {
+      setFetching(true);
+    };
+  }
+
+  useEffect(() => {
+    document.addEventListener('scroll', scrollHandler);
+
+    return function() {
+      document.removeEventListener('scroll', scrollHandler);
+    };
+  }, []);
 
   useEffect(() => {
     fetchKittens();
-  }, []);
+  }, [fetching]);
 
   async function fetchKittens() {
     try {
-      const response = await axios.get<IKitten[]>('https://api.thecatapi.com/v1/images/search?page=1&limit=15&order=Asc');
-      store.setKittens(response.data);
+      const responses = await axios.get<IKitten[]>(`https://api.thecatapi.com/v1/images/search?page=${currentPage}&limit=15&order=Asc`);
+      store.setKittens([...store.kittens, ...responses.data]);
+      setCurrentPage((prevState) => prevState + 1);
     } catch (e) {
       console.log(e);
+    } finally {
+      setFetching(false);
     }
   }
 
